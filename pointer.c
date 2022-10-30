65f94f812e94d9dd64546717534a416c4f43c264
TheWerewolf07 <116957383+TheWerewolf07@users.noreply.github.com>
2022-10-30 11:45:11 +0530

    Add files via upload

---
+++ pointer.c
@@ -0,0 +1,20 @@
+#include<iostream>.
+
+using namespace std;
+class test
+{
+  public;
+  int x,y;
+  test()
+  {
+      x=10;
+      y=20;
+  }
+}
+int main()
+{
+    test t1;
+    test p;
+    // add the value of x and y using pointer varible
+
+}
---
+++ test.2 .c
@@ -0,0 +1,23 @@
+#include< iostream>
+using namespace std;
+int add (int n)
+{
+    int p;
+    if(n==0)
+    {
+        return n;
+    }
+    p=n+add(n-1);
+    return p;
+}
+int main()
+{
+    int main()
+    {
+        int x,z;
+    }
+}
+cout<< "Enter number"<<endl;
+cin>>x;4
+z=add(x);
+cout<<"addition is " <<z;
